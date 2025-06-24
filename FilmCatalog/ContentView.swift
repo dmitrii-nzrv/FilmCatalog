806583f0330cc80d1d6d8342cf06236c55c389f5
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-06-24 23:50:58 +0300

    first commit

---
+++ FilmCatalog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FilmCatalog
+//
+//  Created by Dmitrii Nazarov on 24.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FilmCatalog/FilmCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmCatalogApp.swift
+//  FilmCatalog
+//
+//  Created by Dmitrii Nazarov on 24.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FilmCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
